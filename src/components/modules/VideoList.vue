a4e9bbe5d41e626b2e826076d8ffe9f91f87f8b1
Fan Yang <yangfcm16@gmail.com>
2020-07-25 23:25:48 +0930

    Playlist detail can work

---
+++ src/components/modules/VideoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <app-info-message v-if="!videos || videos.length === 0">There is no video in the playlist</app-info-message>
+    <div v-else class="app-list-container">
+      <h2 class="ui header">Play list videos</h2>
+      <app-video-list-item v-for="(video, index) in videos" :key="index" :video="video"></app-video-list-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import InfoMessage from "../common/InfoMessage";
+import VideoListItem from "./VideoListItem";
+
+export default {
+  components: {
+    appInfoMessage: InfoMessage,
+    appVideoListItem: VideoListItem,
+  },
+  props: ["videos"],
+};
+</script>
+
+<style scoped>
+.app-list-container {
+  max-width: 990px;
+  margin: 0 auto;
+}
+</style>
--- src/components/pages/PlaylistDetail.vue
+++ src/components/pages/PlaylistDetail.vue
@@ -1,10 +1,86 @@
 <template>
   <!-- PlaylistDetail page - Videos in a particular playlist -->
-  <div>Playlist detail</div>
+  <div>
+    <app-blank></app-blank>
+    <app-loader v-if="!error && !playlistDetail"></app-loader>
+    <app-error-message v-if="error">{{ error }}</app-error-message>
+    <div v-if="!error && playlistDetail">
+      <app-video-list :videos="playlistDetail.items"></app-video-list>
+      <app-blank></app-blank>
+      <app-more-button
+        :nextPageToken="playlistDetail.nextPageToken"
+        :isLoadingMore="isLoadingMore"
+        @onClickMore="handleMore($event)"
+      >More videos</app-more-button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapState, mapGetters, mapActions } from "vuex";
+import Blank from "../common/Blank";
+import ErrorMessage from "../common/ErrorMessage";
+import Loader from "../common/Loader";
+import VideoList from "../modules/VideoList";
+import MoreButton from "../modules/MoreButton";
+
+export default {
+  components: {
+    appBlank: Blank,
+    appErrorMessage: ErrorMessage,
+    appLoader: Loader,
+    appVideoList: VideoList,
+    appMoreButton: MoreButton,
+  },
+  data: function () {
+    return {
+      playlistDetail: null,
+      error: "",
+      isLoadingMore: false,
+    };
+  },
+  computed: {
+    ...mapState(["playlist"]),
+    ...mapGetters(["playlistErrorMessage"]),
+    playlistId() {
+      return this.$route.params.id;
+    },
+  },
+  methods: {
+    ...mapActions(["fetchPlaylistDetail"]),
+    async handleMore($event) {
+      if (this.isLoadingMore) return;
+      this.isLoadingMore = true;
+      const nextPageToken = $event;
+      await this.fetchPlaylistDetail([this.playlistId, nextPageToken]);
+      if (this.playlist.playlistError) {
+        this.error = this.playlistErrorMessage;
+      } else {
+        this.playlistDetail = {
+          ...this.playlistDetail,
+          items: this.playlistDetail.items.concat(
+            this.playlist.playlistDetail.items
+          ),
+          nextPageToken: this.playlist.playlistDetail.nextPageToken,
+        };
+        this.error = "";
+      }
+      this.isLoadingMore = false;
+    },
+  },
+  async created() {
+    await this.fetchPlaylistDetail([this.playlistId]);
+    if (this.playlist.playlistError) {
+      if (this.playlist.playlistError.response.data.error.code == 404) {
+        this.$router.push("/not-found");
+        return;
+      }
+      this.error = this.playlistErrorMessage;
+      return;
+    }
+    this.playlistDetail = this.playlist.playlistDetail;
+  },
+};
 </script>
 
 <style scoped></style>
