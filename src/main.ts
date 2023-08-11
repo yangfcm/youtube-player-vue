58a72b849171000cacea0e310d22aebd956b7fa0
yangfcm <yangfcm@outlook.com>
2023-08-11 20:40:43 +0930

    Connect app to vuetify.

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import vuetify from './vuetify'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
