811aa7de19817efe6887ceba48db42a1ac8310d8
yangfcm <yangfcm@outlook.com>
2023-08-19 23:59:36 +0930

    Start to get access token.

--- src/components/GoogleAuthComp.vue
+++ src/components/GoogleAuthComp.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import { onMounted, ref, onUnmounted } from 'vue';
-import { type GsiResponse } from '@/settings/types';
-import { decodeJwtResponse } from '@/settings/utils';
 import { useAuthStore } from '@/stores/auth';
 
 const authStore = useAuthStore()
 const { signin, signout } = authStore;
 
 const gsiScriptLoaded = ref(false);
+const client = ref<any>(null);
 
 const initializeGsi = () => {
   const { google } = window as any;
@@ -16,33 +15,46 @@ const initializeGsi = () => {
     return;
   };
   gsiScriptLoaded.value = true;
-  google.accounts.id.initialize({
+  client.value = google.accounts.oauth2.initTokenClient({
     client_id: import.meta.env.VITE_CLIENT_ID,
-    callback: (response: GsiResponse) => {
-      const decoded = decodeJwtResponse(response.credential);
-      if(decoded === null) {
-        signout();
-        return;
-      }
-      signin({
-        email: decoded.email as string,
-        exp: decoded.exp as number,
-        family_name: decoded.family_name as string,
-        given_name: decoded.given_name as string,
-        locale: decoded.locale as string,
-        name: decoded.name as string,
-        picture: decoded.picture as string,
-      })
+    scope: 'https://www.googleapis.com/auth/userinfo.profile',
+    callback: (res: any) => {
+      console.log(res); // get token.
+      // @TODO: Fetch user info via: https://www.googleapis.com/oauth2/v3/userinfo?access_token=... and proceed with login.
     }
-  });
-  google.accounts.id.renderButton(
-    document.getElementById('google-auth-button'),
-    {
-      theme: 'filled_blue',
-      text: 'continue_with'
-    }
-  )
+  })
+  // google.accounts.id.initialize({
+  //   client_id: import.meta.env.VITE_CLIENT_ID,
+  //   callback: (response: GsiResponse) => {
+  //     const decoded = decodeJwtResponse(response.credential);
+  //     console.log(response.credential, decoded);
+  //     if(decoded === null) {
+  //       signout();
+  //       return;
+  //     }
+  //     signin({
+  //       email: decoded.email as string,
+  //       exp: decoded.exp as number,
+  //       family_name: decoded.family_name as string,
+  //       given_name: decoded.given_name as string,
+  //       locale: decoded.locale as string,
+  //       name: decoded.name as string,
+  //       picture: decoded.picture as string,
+  //     });
+  //   }
+  // });
+  // google.accounts.id.renderButton(
+  //   document.getElementById('google-auth-button'),
+  //   {
+  //     theme: 'filled_blue',
+  //     text: 'continue_with'
+  //   }
+  // )
 };
+
+const logingWithGoogle = () => {
+  client?.value?.requestAccessToken();
+}
 
 onMounted(() => {
   const script = document.createElement("script")
@@ -51,7 +63,6 @@ onMounted(() => {
   script.async = true
   script.id = "google-client-script"
   document.querySelector("body")?.appendChild(script)
-  if(!script) return;
 });
 
 onUnmounted(() => {
@@ -61,7 +72,6 @@ onUnmounted(() => {
 });
 </script>
 
-<template> 
-  <div id="google-auth-button">
-  </div> 
+<template>
+  <button @click="logingWithGoogle">Login with google</button>
 </template>
