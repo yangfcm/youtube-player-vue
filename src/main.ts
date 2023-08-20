564cc0989320d88106cc41644d85212cc52fd6ea
yangfcm <yangfcm@outlook.com>
2023-08-20 23:45:16 +0930

    Save token on sign in and remove token on sign out.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import vuetify from './vuetify'
 import router from './router'
+import pinia from './pinia'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(vuetify)
 app.use(router)
 
