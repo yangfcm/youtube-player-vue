715dc795bf28840c35d3a3f895533d2e8c4aa984
yangfcm <yangfcm@outlook.com>
2023-09-05 20:42:32 +0930

    Add searchResults store.

---
+++ src/stores/searchResults.ts
@@ -0,0 +1,46 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { AsyncStatus } from '@/settings/types'
+import { fetchSearchResultsAPI } from './api'
+import type { AxiosResponse } from 'axios'
+import type { SearchResultsResponse } from './types'
+
+type SearchResultsStore = {
+  error: string
+  status: AsyncStatus
+  results?: SearchResultsResponse
+}
+
+export const useSearchResultsStore = defineStore('searchResults', () => {
+  const searchResults = ref<SearchResultsStore>({
+    error: '',
+    status: AsyncStatus.IDLE,
+  })
+
+  const fetchResults = async (keyword: string, pageToken?: string) => {
+    try {
+      searchResults.value.status = AsyncStatus.LOADING
+      searchResults.value.error = ''
+      const options: Record<string, string> = {}
+      if (pageToken) options.pageToken = pageToken
+      const response: AxiosResponse<SearchResultsResponse> = await fetchSearchResultsAPI(
+        keyword,
+        options,
+      )
+      const currentItems = searchResults.value.results?.items || []
+      searchResults.value.results = {
+        ...response.data,
+        items: [...currentItems, ...response.data.items],
+      }
+      searchResults.value.status = AsyncStatus.SUCCESS
+    } catch (err: any) {
+      searchResults.value.status = AsyncStatus.FAIL
+      searchResults.value.error = err.message
+    }
+  }
+
+  return {
+    fetchResults,
+    searchResultsState: searchResults,
+  }
+})
