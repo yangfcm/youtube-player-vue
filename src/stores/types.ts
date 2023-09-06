9b79707f51adfe2c537e4be83ff2376679799c1a
yangfcm <yangfcm@outlook.com>
2023-09-07 09:25:14 +0930

    Add types for comments and replies.

--- src/stores/types.ts
+++ src/stores/types.ts
@@ -281,3 +281,83 @@ export type SearchResultsResponse = {
   nextPageToken?: string
   prevPageToken?: string
 }
+
+/** Type definition for comments */
+export type CommentSnippet = {
+  etag: string
+  id: string
+  kind: string
+  snippet: {
+    videoId: string
+    totalReplyCount: number
+    isPublic: boolean
+    canReply: boolean
+    topLevelComment: {
+      id: string
+      etag: string
+      kind: string
+      snippet: {
+        authorChannelId: {
+          value: string
+        }
+        authorChannelUrl: string
+        authorDisplayName: string
+        authorProfileImageUrl: string
+        canRate: boolean
+        likeCount: number
+        publishedAt: Date
+        textDisplay: string
+        textOriginal: string
+        updatedAt: Date
+        videoId: string
+        viewerRating: string
+      }
+    }
+  }
+}
+
+export type ReplySnippet = {
+  etag: string
+  id: string
+  kind: string
+  snippet: {
+    authorChannelId: {
+      value: string
+    }
+    authorChannelUrl: string
+    authorDisplayName: string
+    authorProfileImageUrl: string
+    canRate: boolean
+    likeCount: number
+    parentId: string
+    publishedAt: Date
+    textDisplay: string
+    textOriginal: string
+    updatedAt: Date
+    viewerRating: string
+  }
+}
+
+export type CommentResponse = {
+  etag: string
+  kind: string
+  items: CommentSnippet[]
+  nextPageToken?: string
+  pageInfo?: {
+    resultsPerPage?: number
+    totalResults?: number
+  }
+}
+
+export type ReplyResponse = {
+  etag: string
+  kind: string
+  items: ReplySnippet[]
+  nextPageToken?: string
+  pageInfo?: {
+    resultsPerPage?: number
+    totalResults?: number
+  }
+}
+
+export type CommentOrder = 'relevance' | 'time'
