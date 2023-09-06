5d4fd0d5bf7995fbec1e828a24342baee4bbf39e
yangfcm <yangfcm@outlook.com>
2023-09-07 00:16:07 +0930

    Fix video id.

--- src/components/VideoCard.vue
+++ src/components/VideoCard.vue
@@ -3,20 +3,21 @@ import { type VideoMetaSnippetStats } from '@/stores/types';
 import { formatNumber, fromNow } from '@/settings/utils';
 import { PLACEHOLDER_IMAGE_RECTANGLE } from '@/settings/constants';
 
-defineProps<{
+const props = defineProps<{
   video: VideoMetaSnippetStats
 }>()
 
+const videoId = typeof props.video.id === 'string' ? props.video.id : props.video.id.videoId 
 </script>
 
 <template>
   <v-card>
-    <router-link :to="`/video/${video.id}`">
+    <router-link :to="`/video/${videoId}`">
       <v-img :lazy-src="PLACEHOLDER_IMAGE_RECTANGLE" :src="video.snippet.thumbnails.high?.url"></v-img>
     </router-link>
     <v-tooltip :text="video.snippet.title" location="bottom">
       <template v-slot:activator="{props}">
-        <router-link :to="`/video/${video.id}`">
+        <router-link :to="`/video/${videoId}`">
           <v-card-title v-bind="props">{{ video.snippet.title }}</v-card-title>
         </router-link>
       </template>
