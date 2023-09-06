c97397013f8024595e72d789ca6774880cab5ce3
yangfcm <yangfcm@outlook.com>
2023-09-06 23:41:48 +0930

    Complete channel playlists page.

--- src/pages/ChannelPlayLists.vue
+++ src/pages/ChannelPlayLists.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { useChannelPlayLists } from '@/composables/useChannelPlayLists';
+import AppLoader from '@/components/LoaderComp.vue'
+import AppErrorMessage from '@/components/ErrorMessageComp.vue'
+import AppItemsGrid from '@/components/ItemsGrid.vue';
+import AppMoreButton from '@/components/MoreButton.vue';
+import { AsyncStatus } from '@/settings/types';
+
+const route = useRoute()
+const channelId = route.params.id.toString()
+const { playlists, status, error, hasMore, fetchMore } = useChannelPlayLists(channelId)
+
+</script>
 
 <template>
-  channel playlists
+  <app-loader v-if="status === AsyncStatus.LOADING && !playlists.length"></app-loader>
+  <app-error-message v-if="status === AsyncStatus.FAIL" :message="error" class="mb-3"></app-error-message>
+  <app-items-grid :playlists="playlists"></app-items-grid>
+  <app-more-button
+    v-if="hasMore"
+    :loading="status === AsyncStatus.LOADING"
+    @onLoadMore="fetchMore"
+  >
+    More Playlists
+  </app-more-button>
 </template>
