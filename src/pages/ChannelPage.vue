48a07ec084f6274963003f3c1049809b0e3a67e7
yangfcm <yangfcm@outlook.com>
2023-09-06 22:29:15 +0930

    Add channel's banner image.

--- src/pages/ChannelPage.vue
+++ src/pages/ChannelPage.vue
@@ -3,6 +3,7 @@ import { useRoute } from 'vue-router';
 import { useChannelProfile } from '@/composables/useChannelProfile';
 import AppLoader from '@/components/LoaderComp.vue'
 import AppErrorMessage from '@/components/ErrorMessageComp.vue'
+import AppChannelBanner from '@/components/ChannelBanner.vue'
 import { AsyncStatus } from '@/settings/types';
 
 const route = useRoute()
@@ -14,7 +15,10 @@ const { status, error, channelProfile } = useChannelProfile(channelId);
 <template>
   <app-loader v-if="status === AsyncStatus.LOADING && !channelProfile"></app-loader>
   <app-error-message v-if="status===AsyncStatus.FAIL">{{ error }}</app-error-message>
-  <div v-if="status===AsyncStatus.SUCCESS && !!channelProfile">{{ channelProfile.snippet.title }}</div>
+  <div v-if="status===AsyncStatus.SUCCESS && !!channelProfile">
+    <app-channel-banner :imageUrl="channelProfile.brandingSettings?.image?.bannerExternalUrl"></app-channel-banner>
+    {{ channelProfile.snippet.title }}
+  </div>
   <v-tabs align-tabs="title">
     <v-tab :to="`/channel/${channelId}/videos`">Videos</v-tab>
     <v-tab :to="`/channel/${channelId}/playlists`">Playlists</v-tab>
