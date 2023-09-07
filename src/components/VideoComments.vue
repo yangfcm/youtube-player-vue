f8cbe3bfc9ea740fb68642201b068703481e9256
yangfcm <yangfcm@outlook.com>
2023-09-07 17:19:41 +0930

    Show comments on video page.

--- src/components/VideoComments.vue
+++ src/components/VideoComments.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import { useComments } from '@/composables/useComments';
 import AppSortComments from './SortComments.vue'
 import AppAddComment from './AddComment.vue'
+import AppLoader from '@/components/LoaderComp.vue'
+import AppErrorMessage from '@/components/ErrorMessageComp.vue'
+import AppMoreButton from '@/components/MoreButton.vue';
+import { AsyncStatus } from '@/settings/types';
 
-defineProps<{
+const props = defineProps<{
   videoId: string,
 }>()
+
+const { comments, order, status, error, hasMore, fetchMore } = useComments(props.videoId)
+
 </script>
 
 <template>
@@ -16,4 +24,16 @@ defineProps<{
   <div class="my-2">
     <app-add-comment :videoId="videoId"></app-add-comment>
   </div>
+  <app-loader v-if="status===AsyncStatus.LOADING && !comments.length"></app-loader>
+  <app-error-message v-if="status===AsyncStatus.FAIL" :message="error"></app-error-message>
+  <div v-for="comment in comments" :key="comment.id">
+    {{ comment.snippet.topLevelComment.snippet.textOriginal }}
+  </div>
+  <app-more-button
+    v-if="hasMore"
+    :loading="status===AsyncStatus.LOADING"
+    @onLoadMore="fetchMore"
+  >
+    More comments
+  </app-more-button>
 </template>
