edb0836b41c4ff19e8e68cb89b26478a43352a47
yangfcm <yangfcm@outlook.com>
2023-09-11 12:34:50 +0930

    Bring back checkToken plguin.

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import App from './App.vue'
 import vuetify from './vuetify'
 import router from './router'
 import pinia from './pinia'
+import checkToken from './plugins/checkToken'
 
 const app = createApp(App)
 
+app.use(checkToken)
 app.use(pinia)
 app.use(vuetify)
 app.use(router)
